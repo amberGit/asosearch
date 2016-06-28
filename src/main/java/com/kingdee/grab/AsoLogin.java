02f85cadf892a7a5a87e148ff0b7e02225595bc4
jiao_wen <jiao_wen@kingdee.com>
2016-06-28 20:20:24 +0800

    now can login to www.chandashi.com, implement aso keywords ranking file download

--- src/main/java/com/kingdee/grab/AsoLogin.java
+++ src/main/java/com/kingdee/grab/AsoLogin.java
@@ -2,21 +2,16 @@ package com.kingdee.grab;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.kingdee.grab.entity.Geetest;
 import com.kingdee.grab.entity.SearchRelatedResult;
-import com.kingdee.grab.util.DateTransformUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.CookieStore;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
@@ -24,29 +19,24 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.text.ParseException;
+import java.nio.file.Path;
+import java.text.MessageFormat;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
  * 蝉大师登录
  */
 public class AsoLogin {
+    private static final String HOME_PAGE = "http://www.chandashi.com/";
     private static final String SEARCH_URL = "http://www.chandashi.com/search/index.html";
     private static final String SEARCH_RELATED_URL = "http://www.ddashi.com/search/related.html";
-    private static final String GT_URL = "http://www.chandashi.com/valid/gfrist";
-    private static final String LOGIN_URL = "http://www.chandashi.com/User/login";
     private static final String LOGIN_PAGE_URL = "http://www.chandashi.com/user/login.html";
-    private static final String HOME_PAGE = "http://chandashi.com/";
+    private static final String SEARCH_RESULT_URL = HOME_PAGE + "apps/keywordcontent/appId/{0}.html";
+    private static final String ASO_KEYWORDS_RANKING_EXPORT_URL = HOME_PAGE + "/export/index/appid/{0}.html";
+
     public enum PlatformType {
         IOS("store"), ANDROID("android");
 
@@ -63,8 +53,8 @@ public class AsoLogin {
 
 
     private Map<String, String> loginArgsMap;
-    private CloseableHttpClient httpClient;
-    @Deprecated
+    private final CloseableHttpClient httpClient;
+
     public AsoLogin(String email, String password) {
         httpClient = HttpClients.createDefault();
         loginArgsMap = new HashMap<>();
@@ -72,16 +62,6 @@ public class AsoLogin {
         loginArgsMap.put("password", password);
     }
 
-    public AsoLogin(final String cookieFilePath) {
-        StringBuilder cookieStr = new StringBuilder();
-        try {
-            Files.lines(Paths.get(cookieFilePath), StandardCharsets.UTF_8).forEach(line -> cookieStr.append(line).append('\n'));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        CookieStore cookieStore = loadLocalCookies(cookieStr.toString());
-        httpClient = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
-    }
 
     public Map<String, String> search(String keyword, PlatformType platformType) throws Exception {
         HttpGet httpGet = new HttpGet(SEARCH_URL + "?view=web&keyword=" + keyword + "&type=" + platformType.value());
@@ -108,29 +88,34 @@ public class AsoLogin {
         }
         return null;
     }
-
-    private CookieStore loadLocalCookies(final String cookieStr) {
-        String cookieRegex = "(\\S+)\\s\\S+\\s(\\S+)\\s\\S+\\s(?:(\\d+)\\s)?(\\S+)\\s([^\\n]+)";
-        Pattern cookiePattern = Pattern.compile(cookieRegex);
-        Matcher matcher = cookiePattern.matcher(cookieStr);
-        CookieStore cookieStore = new BasicCookieStore();
-        while (matcher.find()) {
-            int matchCount = matcher.groupCount();
-            BasicClientCookie cookie = new BasicClientCookie(matcher.group(matchCount - 1), matcher.group(matchCount));
-            cookie.setDomain(matcher.group(1));
-            cookie.setPath(matcher.group(2));
-            String unixTimeStampStr = matcher.group(matchCount - 2);
-            if (StringUtils.isNotBlank(unixTimeStampStr) && unixTimeStampStr.matches("\\d+")) {
-                try {
-                    Date expiryDate = DateTransformUtil.unixTimestamp2Date(Long.parseLong(unixTimeStampStr));
-                    cookie.setExpiryDate(expiryDate);
-                } catch (ParseException e) {
-                    e.printStackTrace();
+    @Deprecated
+    public void viewSearchDetail(String appId) {
+        String url = MessageFormat.format(SEARCH_RESULT_URL, appId);
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    System.out.println(EntityUtils.toString(entity));
                 }
             }
-            cookieStore.addCookie(cookie);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return cookieStore;
+    }
+
+    public void downloadAsoKeywordsRankingFile(String appId, Path destPath) {
+        Objects.requireNonNull(destPath, "文件路径不能为空");
+        String url = MessageFormat.format(ASO_KEYWORDS_RANKING_EXPORT_URL, appId);
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                HttpEntity entity = response.getEntity();
+                Files.copy(entity.getContent(), destPath);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<SearchRelatedResult> searchRelated(String text) {
@@ -151,9 +136,8 @@ public class AsoLogin {
         return null;
     }
 
-    @Deprecated
     public void login() {
-        HttpPost httpPost = new HttpPost(LOGIN_URL);
+        HttpPost httpPost = new HttpPost(LOGIN_PAGE_URL);
         List<NameValuePair> params = new ArrayList<>();
         String content = loginPage();
         try {
@@ -163,14 +147,7 @@ public class AsoLogin {
             } else {
                 throw new Exception("获取 rand 出错");
             }
-            Geetest geetest = getGeetest();
-            if (geetest != null) {
-                params.add(new BasicNameValuePair("geetest_challenge", geetest.getChallenge()));
-                params.add(new BasicNameValuePair("geetest_seccode", geetest.getGt() + "|jordan"));
-                params.add(new BasicNameValuePair("geetest_validate", geetest.getGt()));
-            } else {
-                throw new Exception("获取geetest 信息出错");
-            }
+
 
             loginArgsMap.entrySet().stream().forEach(entry -> params.add(new BasicNameValuePair(entry.getKey(), entry.getValue())));
             UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(params, "UTF-8");
@@ -178,6 +155,7 @@ public class AsoLogin {
             try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
                 HttpEntity entity = response.getEntity();
                 if (entity != null) {
+                    System.out.println(response.getStatusLine());
                     System.out.println(EntityUtils.toString(entity, "UTF-8"));
                 }
             }
@@ -188,7 +166,6 @@ public class AsoLogin {
 
     }
 
-    @Deprecated
     private String loginPage() {
         HttpGet httpGet = new HttpGet(LOGIN_PAGE_URL);
         try {
@@ -203,7 +180,6 @@ public class AsoLogin {
         }
         return "";
     }
-    @Deprecated
     private String getRand(String content) throws Exception {
         if (StringUtils.isBlank(content)) {
             throw new Exception("没有抓取到主页内容");
@@ -212,24 +188,6 @@ public class AsoLogin {
             Elements randElem = document.select("input[name=rand]");
             return randElem.first().val();
         }
-    }
-    @Deprecated
-    private Geetest getGeetest() {
-        long currentTimestamp = System.currentTimeMillis();
-        String requestParams = "?t=" + currentTimestamp;
-        HttpGet httpGet = new HttpGet(GT_URL + requestParams);
-        try {
-            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-                HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    Gson gson = new Gson();
-                    return gson.fromJson(EntityUtils.toString(entity), Geetest.class);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
 }
--- src/test/java/com/kingdee/grab/AsoLoginTest.java
+++ src/test/java/com/kingdee/grab/AsoLoginTest.java
@@ -4,6 +4,7 @@ import com.kingdee.grab.entity.SearchRelatedResult;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
@@ -11,27 +12,25 @@ import java.util.Map;
  * 蝉大师登录单元测试
  */
 public class AsoLoginTest {
+    private AsoLogin asoLogin = new AsoLogin("wowv58791@163.com", "johnnyven407");
 
     @Ignore
     public void searchRelatedTest() {
-        AsoLogin asoLogin = new AsoLogin("C:\\Users\\Administrator\\Desktop\\cookies.txt");
         List<SearchRelatedResult> searchRelatedResults = asoLogin.searchRelated("51");
     }
-
     @Test
+    public void loginTest() {
+        asoLogin.login();
+        asoLogin.downloadAsoKeywordsRankingFile("564765093", Paths.get("C:\\Users\\Administrator\\Desktop\\test.xls"));
+    }
+    @Ignore
     public void searchTest() {
-//        AsoLogin asoLogin = new AsoLogin("C:\\Users\\Administrator\\Desktop\\cookies.txt");
-        AsoLogin asoLogin = new AsoLogin("E:\\系统\\桌面\\cookies.txt");
         try {
             Map<String, String > searchMap = asoLogin.search("51", AsoLogin.PlatformType.IOS);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    @Ignore
-    public void matchTest() {
-        String str = "123444";
-        System.out.println(str.matches("\\d*"));
-    }
+
 
 }
